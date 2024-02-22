50a074d51262fa6d00d7bc69742caee212edb481
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-02-22 11:42:06 -0600

    final changes for now

--- index.py
+++ index.py
@@ -3,3 +3,21 @@
 from walking import Cow, Donkey, Goat, Llama, Sheep
 from swimming import Duck, Frog, Goldfish, Goose, Trout
 from slithering import Cobra, Copperhead, LeglessLizard, Python, Worm
+
+
+# bertha = Cow("Bertha", "cow", "midday")
+# print(
+#     f"{bertha.name} the {bertha.species} is available to pet during the {bertha.shift} shift"
+# )
+
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama Chow")
+
+miss_fuzz.feed()
+
+daisy = Donkey("Daisy", "donkey", "evening", "hay")
+
+print(daisy)
+
+fang = Cobra("Fang", "snake", "rats")
+
+print(fang)
